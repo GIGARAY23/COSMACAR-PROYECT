c773e6b77cef8cdadedabe5b017f09c0f7824116
geo23ga <geovannigarai41@gmail.com>
2021-05-04 19:19:17 -0500

    HOLA

--- src/app/paginas/servicios/servicios.component.ts
+++ src/app/paginas/servicios/servicios.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../servicios/auth.service';
+import { Router, RouterModule } from '@angular/router';
+import { Manteniminetos } from 'src/app/modelo';
+import { FirestoreService } from '../../services/firestore.service';
 
 @Component({
   selector: 'app-servicios',
@@ -6,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servicios.component.scss'],
 })
 export class ServiciosComponent implements OnInit {
+  mantenimiento: Manteniminetos[] = [];
+  private path = 'mantenimeinto/';
+  constructor(private authService: AuthService,
+    public router: Router,
+    public firestoreService: FirestoreService) { }
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.getProductos();
+  }
+  getProductos() {
+    this.firestoreService.getCollection<Manteniminetos>(this.path).subscribe(
+      (res) => {
+        this.mantenimiento = res;
+        this.firestoreService
+      }
+    );
+  }
 }
